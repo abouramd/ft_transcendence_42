b13008a54961bdc37b102fb1459b1dfc92a82362
abdelhay bouramdane <abouramd@student.1337.ma>
2024-08-16 17:59:48 +0100

    @abouramd

--- requirements/frontend/srcs/js/index.js
+++ requirements/frontend/srcs/js/index.js
@@ -5,34 +5,3 @@ export default class global {
 }
 
 
-
-// /* cursor effect folow */
-//
-//
-// const cursor = document.getElementById("cursor");
-//
-//
-// // follow cursor on mousemove
-// document.addEventListener("mousemove", (e) => {
-//
-//   if (!cursor)
-//     return;
-//
-//   let x = e.pageX;
-//   let y = e.pageY;
-//
-//   cursor.style.top = y + "px";
-//   cursor.style.left = x + "px";
-//   cursor.style.display = "block";
-// });
-//
-// // cursor effects when mouseout
-// document.addEventListener("mouseout", () => {
-//
-//   if (!cursor)
-//     return;
-//
-//   cursor.style.display = "none";
-//   // console.log("mouse out");
-// });
-
